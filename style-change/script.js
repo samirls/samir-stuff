93c7c611e6947dd2cd15902255ec94eba50e6fd7
samirls <samirlaguardiaii@gmail.com>
2022-09-18 10:57:37 -0300

    more responsive buttons

--- style-change/script.js
+++ style-change/script.js
@@ -1,18 +1,20 @@
 let element = document.getElementsByClassName ("font1");
 let background = document.getElementById ("background");
-let toggleStatus = "on";
+let toggleStatus1 = "on";
+let toggleStatus2 = "on";
+let toggleStatus3 = "on";
 
 
 function changeFont() {
-    switch (toggleStatus) {
+    switch (toggleStatus1) {
     case "on":
-        toggleStatus = "off";
+        toggleStatus1 = "off";
         for (i = 0; i < element.length; i++) {
             element[i].style.fontFamily = 'caveat';
             }
         break;
     case "off":
-        toggleStatus = "on";
+        toggleStatus1 = "on";
         for (i = 0; i < element.length; i++) {
             element[i].style.fontFamily = 'combo';
             }
@@ -21,15 +23,15 @@ function changeFont() {
 }
 
 function changeColor() {
-    switch (toggleStatus) {
+    switch (toggleStatus2) {
     case "on":
-        toggleStatus = "off";
+        toggleStatus2 = "off";
         for (i = 0; i < element.length; i++) {
             element[i].style.color = 'red';
             }
         break;
     case "off":
-        toggleStatus = "on";
+        toggleStatus2 = "on";
         for (i = 0; i < element.length; i++) {
             element[i].style.color = 'black';
             }
@@ -39,15 +41,15 @@ function changeColor() {
 }
 
 function changeFontSize() {
-    switch (toggleStatus) {
+    switch (toggleStatus3) {
     case "on":
-        toggleStatus = "off";
+        toggleStatus3 = "off";
         for (i = 0; i < element.length; i++) {
             element[i].style.fontSize = "130%";
             }
         break;
     case "off":
-        toggleStatus = "on";
+        toggleStatus3 = "on";
         for (i = 0; i < element.length; i++) {
             element[i].style.fontSize = "100%";
             }
