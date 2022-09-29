285bd3101901566a048f3bd55b1b6b7097a14e91
samirls <samirlaguardiaii@gmail.com>
2022-09-29 16:17:17 -0300

    traffic light done

---
+++ escala-de-glasgow/script.js
@@ -0,0 +1,9 @@
+function iniciar() {
+    document.getElementById("iniciar").style.visibility = "hidden";
+    document.getElementById("p1").style.visibility = "hidden";
+    document.getElementById("p2").style.visibility = "hidden";
+}
+
+function primeiraPergunta() {
+    
+}
